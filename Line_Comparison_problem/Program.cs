279a6774f3179421505c1ba13158da957fbdab7e
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-04 17:34:26 +0530

    [AKSHAY] added method to calculate lenght of two lines

--- Line_Comparison_problem/Program.cs
+++ Line_Comparison_problem/Program.cs
@@ -18,9 +18,33 @@ namespace Line_Comparison_problem
             Console.WriteLine("Enter the first y2 co-ordinate point: ");
             double y2 = (double)Convert.ToDecimal(Console.ReadLine());
 
-            double length = (double)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double length1 = (double)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 
-            Console.WriteLine("Length of the line is: " + length);
+
+            Console.WriteLine("Enter the first a1 co-ordinate point: ");
+            double a1 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first b1 co-ordinate point: ");
+            double b1 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first a2 co-ordinate point: ");
+            double a2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first b2 co-ordinate point: ");
+            double b2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            double length2 = (double)Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+
+
+            Console.WriteLine("Length of the first line is: " + length1);
+            Console.WriteLine("Length of the second line is: " + length2);
+
+
+            if (length1 == length2)
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
+
             Console.ReadKey();
         }
     }
